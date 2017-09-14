106a7e0666ca082772a49b8c86e6b1d8032e1f6e
Raili Mikksaar <railimikksaar@users.noreply.github.com>
2017-09-14 11:32:31 +0300

    lisatud muutujad ning IF lause

--- esimene.php
+++ esimene.php
@@ -1,3 +1,23 @@
+<?php
+	//muutujad 
+	$myName = "Raili";
+	$myFamilyName = "Mikksaar";
+	
+	//hindan päeva osa, võrdlemine <= väiksem võrdne, >= suuremvõrdne, == võrdne, != mittevõrdne
+	$hourNow = date("H");
+	$partOfDay = "";
+	if ($hourNow < 8){
+		$partOfDay = "varajane hommik";
+	}
+	if ($hourNow >= 8 and $hourNow <16){
+		$partOfDay = "koolipäev";
+	}
+	if ($hourNow >16) {
+		$partOfDay = "vaba aeg";
+	}
+	echo $partOfDay;
+	?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,10 +25,14 @@
 	<title>Hariduslikul eesmärgil loodud leht</title>
 </head>
 <body>
-	<h1>Veebiprogrammeerimine, pealkiri</h1>
+	<h1><?php echo $myName ." " .$myFamilyName; ?>, Veebiprogrammeerimine, pealkiri</h1>
 	<p>Sain sisse logimisega kodus hakkama!   </p>
 	<?php 
-	echo "Algas PHP õppimine.";
+	echo "<p>Algas PHP õppimine.</p>";
+	echo "<p>Täna on ";
+	echo date("d.m.Y") .", lehe avamise hetkel oli kell " .date("H:i:s");
+	echo ", hetkel on " .$partOfDay .".</p>";
+	
 	?>
 </body>
 </html>

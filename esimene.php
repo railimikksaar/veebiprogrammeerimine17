3a3262f10f8f1966303365c81b99e353b688c007
Raili Mikksaar <railimikksaar@users.noreply.github.com>
2017-09-14 10:46:58 +0300

    lisatud esimene php

---
+++ esimene.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Hariduslikul eesmärgil loodud leht</title>
+</head>
+<body>
+	<h1>Veebiprogrammeerimine, pealkiri</h1>
+	<p>Sain sisse logimisega kodus hakkama!   </p>
+	<?php 
+	echo "Algas PHP õppimine.";
+	?>
+</body>
+</html>

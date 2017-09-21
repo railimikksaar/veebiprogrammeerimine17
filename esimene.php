297c8d01820f4cd1c161f1ce28d818196863d56d
Raili Mikksaar <railimikksaar@users.noreply.github.com>
2017-09-21 10:24:07 +0300

    Lisatud massiiv, for, tsükkel ja esimene vorm

--- esimene.php
+++ esimene.php
@@ -2,6 +2,10 @@
 	//muutujad 
 	$myName = "Raili";
 	$myFamilyName = "Mikksaar";
+	$monthNamesEt = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+	//var_dump ($monthNamesEt);
+	//echo $monthNamesEt [8];
+	$monthNow = $monthNamesEt[date("n")-1];
 	
 	//hindan päeva osa, võrdlemine <= väiksem võrdne, >= suuremvõrdne, == võrdne, != mittevõrdne
 	$hourNow = date("H");
@@ -15,8 +19,30 @@
 	if ($hourNow >16) {
 		$partOfDay = "vaba aeg";
 	}
-	echo $partOfDay;
+	//echo $partOfDay;
+	//vanusega tegelemine
+	//var_dump ($_POST);
+	//echo $_POST["birthYear"];
+	$myBirthYear;
+	$ageNotice = "";
+	if (isset($_POST["birthYear"]) and $_POST["birthYear"] !=0){
+		$myBirthYear = $_POST["birthYear"];
+		$myAge = date("Y") - $_POST["birthYear"];
+		$ageNotice = "<p>Te olete umbkaudu " .$myAge ."aastat vana.</p>";
+		
+		$ageNotice .= "<p>Olete elanud järgnevatel aastatel:</p> <ol>";
+		for ($i = $myBirthYear; $i <= date("Y"); $i ++){
+			$ageNotice .= "<li>" .$i ."</li>";
+		}
+		$ageNotice .= "</ol>";
+		
+	}
+	
+	/*for ($i = 0; $i < 5; $i ++){
+		echo "ha";
+	}*/
 	?>
+	
 
 <!DOCTYPE html>
 <html>
@@ -30,9 +56,21 @@
 	<?php 
 	echo "<p>Algas PHP õppimine.</p>";
 	echo "<p>Täna on ";
-	echo date("d.m.Y") .", lehe avamise hetkel oli kell " .date("H:i:s");
+	echo date("d. ") .$monthNow .date(" Y") .", lehe avamise hetkel oli kell " .date("H:i:s");
 	echo ", hetkel on " .$partOfDay .".</p>";
 	
 	?>
+	<h2>Natuke vanusest</h2>
+	<form method="POST">
+	<label>Teie sünniaasta: </label>
+	<input name="birthYear" id="birthYear" type="number" value="<?php echo $myBirthYear; ?>" min="1900" max="2017">
+	<input name="submitBirthYear" type="submit" value="Sisesta">
+	</form>
+	<?php
+	if ($ageNotice !=""){
+		echo $ageNotice;
+	}
+	?>
+	
 </body>
 </html>
